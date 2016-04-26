790d64c35a2ee87adb1f6c1ae7db34275412dd72
Raphael Desoyo <desoyoraphael@gmail.com>
2016-04-26 22:03:24 +0100

    Gulp file fully working. fixed #49 #50 #51 #52 #53

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var gulpSass = require('gulp-sass');
+var gulpUglify = require('gulp-uglify');
+var fs = require('fs');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('JSXtoJS', function () {
+	return browserify('./source/js/app.jsx')
+		.transform('babelify', {presets: ['es2015', 'react']})
+		.bundle()
+		.pipe(fs.createWriteStream('./build/js/app.js'));
+});
+
+gulp.task('SCSStoCSS', function () {
+	return gulp.src('./source/sass/style.scss')
+		.pipe(gulpSass().on('error', gulpSass.logError))
+		.pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('minifyHtml', function () {
+	return gulp.src('./source/index.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('./build/'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('./source/js/app.jsx', ['JSXtoJS']);
+	gulp.watch('./source/sass/style.scss', ['SCSStoCSS']);
+	gulp.watch('./source/index.html', ['minifyHtml']);
+});
+
+gulp.task('default', ['JSXtoJS', 'SCSStoCSS', 'minifyHtml', 'watch']);
+
+
+
